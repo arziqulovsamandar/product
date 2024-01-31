596346fefc66d37e481be3e0928e9d3a18ef7ab2
XasanBaxtiyarovich <xasanavalov787@gmail.com>
2024-01-31 08:47:52 +0500

    added base button

---
+++ src/components/base/base-button.vue
@@ -0,0 +1,16 @@
+<template>
+  <button :type="type" :disabled="disabled">
+    <slot />
+  </button>
+</template>
+
+<script setup>
+import { defineProps } from 'vue';
+
+const { type = 'button', disabled = false } = defineProps({
+  type: String,
+  default: "button",
+  disabled: Boolean,
+  default: false,
+});
+</script>
