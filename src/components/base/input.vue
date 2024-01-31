b7cfa7e0ee3f04f694d8cd8c6659b262509a853a
Savesh1508 <savlatxon20042005@gmail.com>
2024-02-01 00:56:13 +0500

    Added banners

--- src/components/base/input.vue
+++ src/components/base/input.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="flex px-[30px] w-[450px] relative h-[64px] rounded-[50px] shadow-lg">
+  <div class="flex w-[450px] relative h-[64px] rounded-[50px] shadow-lg">
     <input
-      class="outline-none w-full h-full"
+      class="px-[30px] rounded-[50px] outline-none w-full h-full"
       :placeholder="placeholder"
     />
-    <button class="text-white bg-[#3BB77E] hover:bg-[#29A56C] rounded-[50px] w-auto text-[16px] transition font-semibold absolute px-[20px] right-0 h-full">
+    <button class="text-white bg-[#3BB77E] hover:bg-[#29A56C] rounded-[50px] w-auto text-[16px] transition font-semibold absolute px-[32px] right-0 h-full">
       {{ button }}
     </button>
   </div>
@@ -13,6 +13,6 @@
 <script setup lang="ts">
 defineProps<{
   placeholder?: string;
-  button?: string;
+  button: string;
 }>();
 </script>
