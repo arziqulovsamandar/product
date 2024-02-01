6fbe765024ceb32a00f073d3063c629e7a69da6a
arziqulovsamandar <124333211+arziqulovsamandar@users.noreply.github.com>
2024-02-02 01:34:21 +0500

    Update index.ts

--- src/features/products/store/index.ts
+++ src/features/products/store/index.ts
@@ -1,38 +1,38 @@
 import { defineStore } from "pinia";
 
 export const useProductStore = defineStore("productCart", {
-  state: () => ({
-    cartData: [],
-    length: 0,
-  }),
-  actions: {
-    // INCREMENT
-    counterPlus(id, type) {
-      let index = null;
-      let selectArr = null;
+//   state: () => ({
+//     cartData: [],
+//     length: 0,
+//   }),
+//   actions: {
+//     // INCREMENT
+//     counterPlus(id, type) {
+//       let index = null;
+//       let selectArr = null;
 
-      let old = JSON.parse(globalThis?.localStorage?.getItem("myProduct"));
-      if (this.cartData.length === 0 && old != null) {
-        this.cartData = [...old];
-      }
-      this.cartData.forEach((item, i) => {
-        if (item.id === id) {
-          selectArr = item;
-          index = i;
-        }
-      });
+//       let old = JSON.parse(globalThis?.localStorage?.getItem("myProduct"));
+//       if (this.cartData.length === 0 && old != null) {
+//         this.cartData = [...old];
+//       }
+//       this.cartData.forEach((item, i) => {
+//         if (item.id === id) {
+//           selectArr = item;
+//           index = i;
+//         }
+//       });
 
-      if (type === "plus") {
-        selectArr.count += 1;
-      } else {
-        selectArr.count -= 1;
-      }
+//       if (type === "plus") {
+//         selectArr.count += 1;
+//       } else {
+//         selectArr.count -= 1;
+//       }
 
-      this.cartData.splice(index, 1, selectArr);
-      this.saveProduct();
-    },
-  },
-  getters: {},
+//       this.cartData.splice(index, 1, selectArr);
+//       this.saveProduct();
+//     },
+//   },
+//   getters: {},
 });
 import { createPinia } from "pinia";
 
