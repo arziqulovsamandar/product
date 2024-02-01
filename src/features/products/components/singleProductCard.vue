e963306a44943c02619030652920655c088f8bc1
MrUmarDev <mrumardev@gmail.com>
2024-02-01 23:53:03 +0500

    Single Product Card

---
+++ src/features/products/components/singleProductCard.vue
@@ -0,0 +1,84 @@
+<template>
+  <div class="main w-[280px] relative p-5 justify-around border-2 border-slate-200 hover:shadow-lg shadow-slate-800 duration-500 ease-in-out cursor-pointer rounded-2xl h-auto m-2">
+    <div class="bg-blue-300 text-white w-[22%] h-[6%] absolute top-0 left-0 text-center rounded-br-2xl rounded-tl-xl">
+      13%
+    </div>
+    <div class="bg-primary text-white w-[22%] h-[6%] absolute top-0 right-0 text-center rounded-bl-2xl rounded-tr-xl">
+      Sale
+    </div>
+    <div>
+      <img class="mx-auto mb-2" src="../../../assets/img/product-2-1.jpg" alt="">
+    </div>
+    <div>
+      <p style='font-size: smaller; color: gray'>Fresh Fruit</p>
+    </div>
+    <div>
+      <h1 class="font-bold text-lg text-gray-600 mb-3">Seeds of Change Organic Red Rice</h1>
+    </div>
+    <div class="text-slate-400 text-sm mb-2">
+      <i class='bx bx-star '></i>
+      <i class='bx bx-star'></i>
+      <i class='bx bx-star'></i>
+      <i class='bx bx-star'></i>
+      <i class='bx bx-star'></i>
+      <span class="ml-2">0</span>
+    </div>
+    <div class='mb-3'>
+      <p style='color: gray'>By <a href="http://localhost:3000/" style="color: #46d573; font-size: larger; opacity: 100%">NestFood</a></p>
+    </div>
+    <div class="flex text-primary font-bold gap-3 items-baseline mb-2">
+      <span class="text-[20px]">$28.95</span>
+      <s class="text-slate-400">$<s class="text-[13px] text-primary">32.80</s></s>
+      <button @click="addToCart" class="bg-primary text-white px-4 py-2 rounded-md flex items-center ml-8 opacity-100 hover:opacity-100">
+        <i class="bx bx-cart-alt mr-2"></i> Add
+      </button>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { useProductStore } from "../store";
+
+const productCart = useProductStore();
+const props = defineProps({
+  data: Object,
+});
+
+import { ref, onMounted } from 'vue';
+
+const count = ref(0);
+
+function increment() {
+  count.value += 1;
+  productCart.counterPlus(props.data?.id, "plus");
+}
+
+function decrement() {
+  if (count.value <= 1) {
+    return;
+  }
+  count.value -= 1;
+  productCart.counterPlus(props.data?.id, "minus");
+}
+
+function addToCart() {
+  productCart.addToCart(props.data);
+}
+
+onMounted(() => {
+  count.value = props.data?.count || 1;
+});
+</script>
+
+<style scoped>
+.main:hover .cover {
+  opacity: 1;
+  visibility: visible;
+  top: 45%;
+}
+
+.main:hover {
+  box-shadow: none;
+  border: none;
+}
+</style>
