fcd7abae571198773ba5103f57b4ba66bc0a624b
Savesh1508 <savlatxon20042005@gmail.com>
2024-02-11 18:57:55 +0500

    Added i18n to About page

--- src/features/users/components/About/ourTeam.vue
+++ src/features/users/components/About/ourTeam.vue
@@ -2,20 +2,13 @@
     <div class="w-full flex justify-center">
     <div class="flex mt-10 justify-between flex-wrap lg:flex-nowrap max-w-[1430px] w-full h-fit">
         <div class="p-[12px] w-[100%] lg:w-[40%] ml-[15px] flex flex-col items-center lg:items-start text-center lg:text-left">
-            <p class="text-main-100 text-[24px] mb-[10px] font-semibold">Our Team</p>
-            <p class="text-fortitle-200 text-[30px] mb-[25px] font-semibold">Meet Our Expert Team</p>
-    
+            <p class="text-main-100 text-[24px] mb-[10px] font-semibold">{{ t('about.team.title') }}</p>
+            <p class="text-fortitle-200 text-[30px] mb-[25px] font-semibold">{{ t('about.team.subtitle') }}</p>
+
             <p class="text-fortitle-100 text-[16px] mb-[15px] ">
-                Proin ullamcorper pretium orci. Donec necscele risque leo. <br> 
-                Nam massa dolor imperdiet neccon sequata congue idsem. <br> 
-                Maecenas malesuada faucibus finibus.
+                {{ t('about.team.text') }}
             </p>
-            <p class="text-fortitle-100 text-[16px] mb-[24px]">
-                Proin ullamcorper pretium orci. Donec necscele risque leo. <br> 
-                Nam massa dolor imperdiet neccon sequata congue idsem. <br> 
-                Maecenas malesuada faucibus finibus.
-            </p>
-            <button class="text-white bg-main-100 hover:text-black hover:bg-[#FDC041] w-fit py-3 px-[15px] rounded-md font-semibold">View All Members</button>
+            <button class="text-white bg-main-100 hover:text-black hover:bg-[#FDC041] w-fit py-3 px-[15px] rounded-md font-semibold">{{ t('about.team.btn') }}</button>
         </div>
 
         <div class="flex w-[100%] lg:w-[60%] h-fit mr-0 lg:mr-[15px] flex-wrap 2xs:flex-nowrap">
@@ -24,8 +17,8 @@
                     <img class="rounded-xl " src="https://themepanthers.com/wp/nest/d1/wp-content/uploads/2022/05/about-6.png" alt="">
                 </div>
                 <div class=" border-white border bottom-[-13%] shadow-lg bg-white w-[60%] absolute rounded-xl">
-                    <p class="text-center text-[24px] font-semibold text-fortitle-200 mt-5">H.Merinda</p>
-                    <p class="text-center text-lg text-fortitle-100 mt-1">CEO & Co-Founder</p>
+                    <p class="text-center text-[24px] font-semibold text-fortitle-200 mt-5">{{ t('about.team.members.member1.name') }}</p>
+                    <p class="text-center text-lg text-fortitle-100 mt-1">{{ t('about.team.members.member1.job') }}</p>
                     <div class="flex justify-center items-center mt-5 mb-5">
                         <a href="#" class="no-underline text-fortitle-200 ">
                             <i class='bx bxl-facebook text-main-100  hover:text-[#CB2356]'></i>
@@ -47,8 +40,8 @@
                     <img class="rounded-xl" src="https://themepanthers.com/wp/nest/d1/wp-content/uploads/2022/05/about-8.png" alt="">
                 </div>
                 <div class="border-white border bottom-[-13%] shadow-lg bg-white w-[60%] absolute rounded-xl">
-                    <p class="text-center text-[24px] font-semibold text-fortitle-200 mt-5">Dilan Specter</p>
-                    <p class="text-center text-lg text-fortitle-100 mt-1">Head Engineer</p>
+                    <p class="text-center text-[24px] font-semibold text-fortitle-200 mt-5">{{ t('about.team.members.member2.name') }}</p>
+                    <p class="text-center text-lg text-fortitle-100 mt-1">{{ t('about.team.members.member2.job') }}</p>
                     <div class="flex justify-center items-center mt-5 mb-5">
                         <a href="#" class="no-underline text-fortitle-200">
                             <i class='bx bxl-facebook text-main-100  hover:text-[#CB2356]'></i>
@@ -69,6 +62,11 @@
     </div>
     </div>
 </template>
+
+<script setup>
+import { useI18n } from "vue-i18n";
+const { t } = useI18n();
+</script>
 
 <style scoped>
 .main:hover .main{
